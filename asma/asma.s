4be115a8cd8db9293dc5472810b6b89fff7e5713
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-03-09 12:33:42 +0100

    Finish asma

--- asma/asma.s
+++ asma/asma.s
@@ -1,19 +1,20 @@
-	.file "asma.s"
+	.ident "Lorenz Leutgeb <u1127842@student.tuwien.ac.at>"
 
 	.data
 	.section .rodata
 	.align 16
+
 asma_mask:
 	.byte 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20
 
 	.text
 	.globl asma
 	.type asma, @function
+
 asma:
-	movdqa asma_mask, %xmm1		# Initialize xmm1
-	movdqu (%rdi), %xmm0		# Load target string from memory
-	pcmpeqb %xmm1, %xmm0		# Replace all spaces with 0xff
-	pmovmskb %xmm0, %rcx		# Collapse XMM to a mask of MSBs
-	popcnt %rcx, %rcx		# Count number of ones in %rcx and store it to %rcx
-	leave
+	movdqa		asma_mask, %xmm1	# Initialize xmm1
+	movdqu		(%rdi), %xmm0		# Load target string from memory
+	pcmpeqb		%xmm1, %xmm0		# Replace all spaces with 0xff
+	pmovmskb	%xmm0, %rax		# Collapse XMM to a mask of MSBs
+	popcnt		%rax, %rax		# Count number of ones in %rcx and store it to %rcx
 	ret
--- asma/main.c
+++ asma/main.c
@@ -2,14 +2,6 @@
 
 extern int asma(char*);
 
-/*int asma(char *s) {
-	int c = 0, i;
-	for (i = 0; i < 16; i++)
-		if (s[i] == ' ')
-			c++;
-	return c;
-}*/
-
 int main(int argc, char** argv) {
-	exit(asma("    "));
+	exit(asma(argv[0]));
 }
