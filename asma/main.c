5fffe422de3e9f0bd4915bcf3b096ad48751ffd8
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-03-11 01:09:48 +0100

    Add output to asma

--- asma/main.c
+++ asma/main.c
@@ -1,7 +1,22 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 extern int asma(char*);
 
+int asma_ref(char *s)
+{
+	int c = 0;
+	int i;
+	for (i = 0; i < 16; i++)
+		if (s[i] == ' ')
+			c++;
+
+	return c;
+}
+
 int main(int argc, char** argv) {
-	exit(asma(argv[0]));
+	if (asma(argv[1]) == asma_ref(argv[1]))
+		printf("ok\n");
+	else
+		printf(":(\n");
+	return 0;
 }
