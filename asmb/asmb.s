809f2eae91b95622c56c419b26beb2e4ca01ebb7
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-03-11 08:38:49 +0100

    Save one instruction in asmb

--- asmb/asmb.s
+++ asmb/asmb.s
@@ -1,16 +1,12 @@
 	.ident "Lorenz Leutgeb <lorenz.leutgeb@student.tuwien.ac.at>"
-
 	.data
 	.section .rodata
-	.align 16
-
 asmb_mask:
+	.align 16
 	.byte 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20
-
 	.text
 	.globl asmb
 	.type asmb, @function
-
 asmb:
 	movdqa		asmb_mask, %xmm8
 	leaq		0, %rax
@@ -27,14 +23,13 @@ loop:
 	loop		loop
 last:
 	andq		$15, %rsi
-	jz		end
-	xorq		$63, %rsi
-	leaq		1 (%rsi), %rcx
+	jz		ret
+	leaq		-63 (%rsi), %rcx
 	movdqu		(%rdi), %xmm9
 	pcmpeqb		%xmm8, %xmm9
 	pmovmskb	%xmm9, %r11
 	shlq		%cl, %r11
 	popcnt		%r11, %r11
 	leaq		(%rax, %r11), %rax
-end:
+ret:
 	ret
