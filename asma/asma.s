eab8b75f19cfd9b97ae56c9669c7fefe39b1cc64
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-03-11 08:39:15 +0100

    Remove comments in asma

--- asma/asma.s
+++ asma/asma.s
@@ -1,20 +1,16 @@
 	.ident "Lorenz Leutgeb <lorenz.leutgeb@student.tuwien.ac.at>"
-
 	.data
 	.section .rodata
-	.align 16
-
 asma_mask:
+	.align 16
 	.byte 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20
-
 	.text
 	.globl asma
 	.type asma, @function
-
 asma:
-	movdqa		asma_mask, %xmm8	# Initialize xmm8
-	movdqu		(%rdi), %xmm9		# Load target string from memory
-	pcmpeqb		%xmm8, %xmm9		# Replace all spaces with 0xff
-	pmovmskb	%xmm9, %rax		# Collapse XMM to a mask of MSBs
-	popcnt		%rax, %rax		# Count number of ones in %rcx and store it to %rcx
+	movdqa		asma_mask, %xmm8
+	movdqu		(%rdi), %xmm9
+	pcmpeqb		%xmm8, %xmm9
+	pmovmskb	%xmm9, %rax
+	popcnt		%rax, %rax
 	ret
